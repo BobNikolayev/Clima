34c7c9ce1866f5076903a283f695e09a31505506
Destroyer <bnikolayev993@gmail.com>
2021-02-13 15:54:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int ab;
+
+        boolean r;
+
+        ab = hui(a,b);
+
+        System.out.println(ab);
+
+    }
+
+    public static int sum (int a, int b) {
+        return a + b;
+   }
+
+    public static int hui (int a, int b){
+        return a * b;
+    }
+
+
+
+}
